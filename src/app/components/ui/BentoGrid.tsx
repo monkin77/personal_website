3bdbafd8521996c97c260f7777e2161adc9acb88
Monkin <monkinsmurf@gmail.com>
2025-01-17 16:39:10 +0000

    Add: Finished refactor of BentoGrid

--- src/app/components/ui/BentoGrid.tsx
+++ src/app/components/ui/BentoGrid.tsx
@@ -14,7 +14,44 @@ import { aboutGridItems as gridItems } from "@/app/data";
 
 const animationList = [compEngAnimData, confettiAnimData];
 
+const contactEmail = "test@gmail.com";
+
+const BentoGridItemWrapper = ({
+    className,
+    innerClassName,
+    children,
+}: {
+    className?: string;
+    innerClassName?: string;
+    children?: React.ReactNode;
+}) => {
+    return (
+        <div
+            className={cn(
+                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
+                className
+            )}
+            style={{
+                background: "rgb(2, 0, 36)",
+                backgroundColor:
+                    "linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(59, 59, 68, 1) 26%, rgba(93, 108, 111, 1) 100%)",
+            }}
+        >
+            <div className={cn("h-full", innerClassName)}>{children}</div>
+        </div>
+    );
+};
+
 export const BentoGrid = ({ className }: { className?: string }) => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        // TODO: Get the email from .env file
+        navigator.clipboard.writeText(contactEmail);
+
+        setCopied(true);
+    };
+
     return (
         <div
             className={cn(
@@ -70,7 +107,6 @@ export const BentoGrid = ({ className }: { className?: string }) => {
                 </div>
             </BentoGridItemWrapper>
 
-
             {/* Grid Item 3 */}
             <BentoGridItemWrapper className="lg:col-span-2 md:col-span-3 md:row-span-2">
                 <div className="group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 justify-center">
@@ -112,214 +148,109 @@ export const BentoGrid = ({ className }: { className?: string }) => {
                 </div>
             </BentoGridItemWrapper>
 
-            
-        </div>
-    );
-};
-
-const BentoGridItemWrapper = ({
-    className,
-    innerClassName,
-    children,
-}: {
-    className?: string;
-    innerClassName?: string;
-    children?: React.ReactNode;
-}) => {
-    return (
-        <div
-            className={cn(
-                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
-                className
-            )}
-            style={{
-                background: "rgb(2, 0, 36)",
-                backgroundColor:
-                    "linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(59, 59, 68, 1) 26%, rgba(93, 108, 111, 1) 100%)",
-            }}
-        >
-            <div className={cn("h-full", innerClassName)}>{children}</div>
-        </div>
-    );
-};
-
-const contactEmail = "test@gmail.com";
-
-export const BentoGridItem = ({
-    className,
-    title,
-    description,
-    header,
-    icon,
-    id,
-    img,
-    animIdx,
-    imgClassName,
-    titleClassName,
-    spareImg,
-}: {
-    className?: string;
-    title?: string | React.ReactNode;
-    description?: string | React.ReactNode;
-    header?: React.ReactNode;
-    icon?: React.ReactNode;
-    id: number;
-    img?: string;
-    animIdx?: number;
-    imgClassName?: string;
-    titleClassName?: string;
-    spareImg?: string;
-}) => {
-    const [copied, setCopied] = useState(false);
-
-    const handleCopy = () => {
-        // TODO: Get the email from .env file
-        navigator.clipboard.writeText(contactEmail);
-
-        setCopied(true);
-    };
-
-    return (
-        <div
-            className={cn(
-                "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
-                className
-            )}
-            style={{
-                background: "rgb(2, 0, 36)",
-                backgroundColor:
-                    "linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(59, 59, 68, 1) 26%, rgba(93, 108, 111, 1) 100%)",
-            }}
-        >
-            <div
-                className={`${id === 6 ? "flex justify-center" : ""} ${
-                    id === 1 ? "relative" : ""
-                } h-full`}
-            >
+            {/* Grid Item 4 */}
+            <BentoGridItemWrapper className="lg:col-span-2 md:col-span-3 md:row-span-1">
                 <div className="w-full h-full absolute">
-                    {img && (
+                    {gridItems[3].img && (
                         <img
-                            src={img}
-                            alt={img}
-                            className={cn(
-                                imgClassName,
-                                "object-cover" // , object-center"
-                            )}
+                            src={gridItems[3].img}
+                            alt={gridItems[3].img}
+                            className="object-cover" // , object-center"
                         />
-                    )}
-
-                    {animIdx != null && [1].includes(id) && (
-                        <div className="bottom-1 h-[95%]">
-                            <Lottie
-                                options={{
-                                    loop: true,
-                                    autoplay: true,
-                                    animationData: animationList[animIdx],
-                                    rendererSettings: {
-                                        /* preserveAspectRatio:
-                                            "xMidYMid slice", */
-                                    },
-                                }}
-                                speed={0.75}
-                            />
-                        </div>
                     )}
                 </div>
 
-                <div
-                    className={`absolute right-0 -bottom-5 ${
-                        id === 5 && "w-full opacity-80"
-                    }`}
-                >
-                    {spareImg && (
+                <div className="absolute right-0 -bottom-5">
+                    {gridItems[3].spareImg && (
                         <img
-                            src={spareImg}
-                            alt={spareImg}
+                            src={gridItems[3].spareImg}
+                            alt={gridItems[3].spareImg}
                             className="object-cover, object-center w-full h-full"
                         />
                     )}
                 </div>
 
-                {id === 6 && (
-                    // Render animation
-                    <BackgroundGradientAnimation />
-                )}
+                <div className="group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 justify-start">
+                    <div className="font-sans font-bold text-lg lg:text-3xl z-10 w-full">
+                        {gridItems[3].title}
+                    </div>
+                </div>
+            </BentoGridItemWrapper>
 
-                <div
-                    className={cn(
-                        titleClassName,
-                        "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            {/* Grid Item 5 */}
+            <BentoGridItemWrapper className="md:col-span-3 md:row-span-2">
+                <div className="w-full h-full absolute">
+                    {gridItems[4].img && (
+                        <img
+                            src={gridItems[4].img}
+                            alt={gridItems[4].img}
+                            className="absolute right-0 bottom-0 md:w-96 w-60 object-cover"
+                        />
                     )}
-                >
+                </div>
+
+                <div className="absolute right-0 -bottom-5 w-full opacity-80">
+                    {gridItems[4].spareImg && (
+                        <img
+                            src={gridItems[4].spareImg}
+                            alt={gridItems[4].spareImg}
+                            className="object-cover, object-center w-full h-full"
+                        />
+                    )}
+                </div>
+
+                <div className="group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5
+                    lg:p-10 justify-center md:justify-start lg:justify-center">
                     <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
-                        {description}
+                        {gridItems[4].description}
                     </div>
 
                     <div className="font-sans font-bold text-lg lg:text-3xl z-10 w-full">
-                        {title}
+                        {gridItems[4].title}
+                    </div>
+                </div>
+            </BentoGridItemWrapper>
+
+            {/* Grid Item 6 */}
+            <BentoGridItemWrapper
+                className="lg:col-span-2 md:col-span-3 md:row-span-1"
+                innerClassName="flex justify-center"
+            >
+                <BackgroundGradientAnimation />
+
+                <div
+                    className="justify-center md:max-w-full max-w-60 text-center group-hover/bento:translate-x-2 transition duration-200 relative md:h-full
+                    min-h-40 flex flex-col px-5 p-5 lg:p-10"
+                >
+                    <div className="font-sans font-bold text-lg lg:text-3xl z-10 w-full">
+                        {gridItems[5].title}
                     </div>
 
-                    {id === 2 && <GridGlobe />}
-
-                    {id === 3 && (
-                        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-                            <div className="flex flex-col gap-3 lg:gap-8">
-                                {leftTechStack.map((item, idx) => (
-                                    <span
-                                        key={idx}
-                                        className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
-                                    >
-                                        {item}
-                                    </span>
-                                ))}
-
-                                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
-                            </div>
-
-                            <div className="flex flex-col gap-3 lg:gap-8">
-                                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
-
-                                {rightTechStack.map((item, idx) => (
-                                    <span
-                                        key={idx}
-                                        className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132e]"
-                                    >
-                                        {item}
-                                    </span>
-                                ))}
-                            </div>
-                        </div>
-                    )}
-
-                    {id === 6 && (
-                        <div className="mt-5 relative">
-                            <div className={`absolute -bottom-5 right-0`}>
-                                <Lottie
-                                    options={{
-                                        loop: copied,
-                                        autoplay: copied,
-                                        animationData: confettiAnimData,
-                                        rendererSettings: {
-                                            preserveAspectRatio:
-                                                "xMidYMid slice",
-                                        },
-                                    }}
-                                />
-                            </div>
-
-                            <MagicButton
-                                title={
-                                    copied ? "Email copied!" : "Copy my email"
-                                }
-                                icon={<IoCopyOutline />}
-                                position="left"
-                                otherClasses="!bg-[#161a31]"
-                                onClick={handleCopy}
+                    <div className="mt-5 relative">
+                        <div className={`absolute -bottom-5 right-0`}>
+                            <Lottie
+                                options={{
+                                    loop: copied,
+                                    autoplay: copied,
+                                    animationData: confettiAnimData,
+                                    rendererSettings: {
+                                        preserveAspectRatio: "xMidYMid slice",
+                                    },
+                                }}
                             />
                         </div>
-                    )}
+
+                        <MagicButton
+                            title={copied ? "Email copied!" : "Copy my email"}
+                            icon={<IoCopyOutline />}
+                            position="left"
+                            otherClasses="!bg-[#161a31]"
+                            onClick={handleCopy}
+                        />
+                    </div>
                 </div>
-            </div>
+            </BentoGridItemWrapper>
         </div>
     );
 };
+
