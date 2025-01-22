f70d228068f85f0323cfac63a340aa38901b9849
Monkin <monkinsmurf@gmail.com>
2025-01-22 17:47:52 +0000

    Add: Background to Resume + simple hero section

--- src/app/resume/ExtendedResume.tsx
+++ src/app/resume/ExtendedResume.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import AvatarSlider from "../components/AvatarSlider";
 
 const ExtendedResume = () => {
-  return (
-    <div className='min-h-screen'>
-        Extended Resume
-    </div>
-  )
-}
+    return (
+        <div className="min-h-screen">
+            <div className="flex justify-center relative mt-20 mb-5 md:mb-10 z-10 flex-col md:flex-row">
+                <div className="flex flex-col items-center justify-center relative h-full">
+                    <AvatarSlider />
+                </div>
+
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60] flex flex-col items-center justify-center md:ml-10 mt-2 md:mt-0">
+                    <h1 className="uppercase heading tracking-wider text-center text-blue-100 text-2xl">
+                        João Mesquita
+                    </h1>
+
+                    {/* <TextGenerateEffect
+                        className="text-center"
+                        textClassName="md:text-3xl lg:text-4xl"
+                        words="Welcome to João Mesquita's (Monkin) Habitat :)"
+                        duration={0.5}
+                        colorShiftIdx={4}
+                        filter={true}
+                    /> */}
+
+                    <p className="text-center md:tracking-wider text-sm md:text-base lg:text-xl mt-2">
+                        {"Computer Engineer | Researcher | Passionate Learner"}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ExtendedResume;
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,11 +4,14 @@ import FloatingDockWrapper from "../components/ui/FloatingDock/Wrapper";
 import { FaArrowLeft } from "react-icons/fa";
 import InvertButton from "../components/ui/Buttons/InvertButton";
 import Link from 'next/link';
+import { BackgroundBeams } from "../components/ui/Bg/BackgroundBeams";
 
 const Resume = () => {
 
     return (
         <main className="relative bg-background flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+            <BackgroundBeams />
+
             <div className="max-w-[90rem] w-full my-10 min-h-screen relative">
                 <div className="sticky top-5">
                     <Link href="/">
