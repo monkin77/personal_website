5709216b34a3e5d105006af950aa562991ca9e62
Monkin <monkinsmurf@gmail.com>
2025-01-26 15:22:55 +0000

    Minified avatar images

--- src/app/components/ui/ImagesSlider.tsx
+++ src/app/components/ui/ImagesSlider.tsx
@@ -143,6 +143,7 @@ export const ImagesSlider = ({
             exit={direction === "up" ? "upExit" : "downExit"}
             variants={slideVariants}
             className="image h-full w-full absolute inset-0 object-cover object-center"
+            alt={`avatar ${currentIndex}`}
           />
         </AnimatePresence>
       )}
